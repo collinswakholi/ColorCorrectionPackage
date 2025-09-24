3ab31fcb4e2027c7c360d6be47d023db53df8c18
Collins Wakholi <wcoln@yahoo.com>
2025-09-24 15:09:54 -0700

    Update workflow to auto-publish on setup.py changes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ColorCorrectionPipeline",
-    version="1.1.8",
+    version="1.1.9",
     author="Collins Wakholi, Devin A. Rippner",
     author_email="wcoln@yahoo.com, devinrippner@gmail.com",
     description="A Stepwise color‐correction pipeline with flat‐field, gamma, white‐balance, and color‐correction stages.",
