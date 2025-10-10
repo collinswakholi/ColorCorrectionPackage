a096d7303401c8ad8b2b03d5400d4271acf5ec7d
Collins Wakholi <wcoln@yahoo.com>
2025-10-10 09:11:46 -0700

    Trigger PyPI publish workflow for v1.3.2

--- ColorCorrectionPipeline/__version__.py
+++ ColorCorrectionPipeline/__version__.py
@@ -4,3 +4,5 @@ __version__ = "1.3.2"
 __version_info__ = tuple(int(i) for i in __version__.split("."))
 
 __all__ = ["__version__", "__version_info__"]
+
+# Release 1.3.2: opencv-contrib-python dependency fix + enhanced utility functions
