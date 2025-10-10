3ec5f9058078fa5c3d2a866737563cc5aa41dce3
Collins Wakholi <wcoln@yahoo.com>
2025-10-10 08:50:37 -0700

    Release v1.3.2: Add opencv-contrib-python dependency for mcc module

--- ColorCorrectionPipeline/__version__.py
+++ ColorCorrectionPipeline/__version__.py
@@ -1,6 +1,6 @@
 """Version information for ColorCorrectionPipeline package."""
 
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 __version_info__ = tuple(int(i) for i in __version__.split("."))
 
 __all__ = ["__version__", "__version_info__"]
